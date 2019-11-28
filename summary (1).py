6f2dbcd1e0bd130f6d0b106a4666f3ae94bf7c39
Pranav Nayak <34138578+pranav1001@users.noreply.github.com>
2019-11-28 18:46:22 +0530

    Add files via upload

---
+++ summary (1).py
@@ -0,0 +1,71 @@
+import bs4 as bs
+import urllib.request
+import re
+import nltk
+nltk.download('stopwords')
+import heapq
+
+scraped_data = urllib.request.urlopen('https://en.wikipedia.org/wiki/Artificial_intelligence')
+article = scraped_data.read()
+
+parsed_article = bs.BeautifulSoup(article,'lxml')
+
+paragraphs = parsed_article.find_all('p')
+
+article_text = ""
+
+for p in paragraphs:
+    article_text += p.text
+    
+# Removing Square Brackets and Extra Spaces
+article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+article_text = re.sub(r'\s+', ' ', article_text)
+
+# Removing special characters and digits
+formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+
+sentence_list = nltk.sent_tokenize(article_text)
+
+stopwords = nltk.corpus.stopwords.words('english')
+
+word_frequencies = {}
+for word in nltk.word_tokenize(formatted_article_text):
+    if word not in stopwords:
+        if word not in word_frequencies.keys():
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
+
+maximum_frequncy = max(word_frequencies.values())
+
+for word in word_frequencies.keys():
+    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+    
+sentence_scores = {}
+for sent in sentence_list:
+    for word in nltk.word_tokenize(sent.lower()):
+        if word in word_frequencies.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
+
+
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+summary = ' '.join(summary_sentences)
+print(summary)
+
+text_file = open("input.txt", "w")
+
+text_file.write(str(article))
+
+text_file.close()
+
+text_file = open("Output.txt", "w")
+
+text_file.write(str(summary))
+
+text_file.close()
